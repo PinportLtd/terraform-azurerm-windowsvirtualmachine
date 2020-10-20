ec8a2d9ac8d5d33f86b96afc7355f0e8be6f5cc4
Russell Maycock <Russell.Maycock@pinport.io>
2020-10-20 15:53:09 +0100

    changed script

--- Azure/VirtualMachine/run-tests.sh
+++ Azure/VirtualMachine/run-tests.sh
@@ -3,12 +3,14 @@
 set -e
 
 # ensure dependencies
-dep ensure
-
+#dep ensure
+cd ./test/
+go mod init github.com/PinportLtd/TerraformModules
+go mod vendor
 # set environment variables
 # export TF_VAR_service_principal_client_id=$SERVICE_PRINCIPAL_CLIENT_ID
 # export TF_VAR_service_principal_client_secret=$SERVICE_PRINCIPAL_CLIENT_SECRET
 
 # run test
-go test -v ./test/ -timeout 30m | tee test_output.log
+go test -v -timeout 30m | tee test_output.log
 terratest_log_parser -testlog test_output.log -outputdir test_output
