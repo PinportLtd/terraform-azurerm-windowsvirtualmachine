7c7d8c352516f21c5248d0164d319e81a24b5f69
Russell Maycock <Russell.Maycock@pinport.io>
2020-10-22 14:30:24 +0100

    run-test sorted

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
 set -e
+az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID 
+az account set -s $ARM_SUBSCRIPTION_ID
+SUBTRUE=$(az account show --query "isDefault")
+SUBID=$(az account show --query "id")
+SUBID=$(sed -e 's/^"//' -e 's/"$//' <<<"$SUBID")
+if [ "$SUBTRUE" -eq "true" ] -&& [ "$SUBID" -eq "$ARM_SUBSCRIPTION_ID" ] ; then
+echo "they are true"
+else
+    echo "they are not true"
+fi
+
+# Run the tests
+cd ./test/
+go test -v -timeout 30m | tee test_output.log
+terratest_log_parser -testlog test_output.log -outputdir test_output
+
+
+
+
+else
+    echo "Not in the correct subscription, exiting"
+fi
+#verifiy correct subscription
 
 # ensure dependencies
 #dep ensure
-cd ./test/
+
 # go mod init github.com/PinportLtd/TerraformModules
 # go mod vendor
 # set environment variables
@@ -12,5 +35,3 @@ cd ./test/
 # export TF_VAR_service_principal_client_secret=$SERVICE_PRINCIPAL_CLIENT_SECRET
 
 # run test
-go test -v -timeout 30m | tee test_output.log
-terratest_log_parser -testlog test_output.log -outputdir test_output
