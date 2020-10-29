728aecd1c9ca265c4a437307008685c45b641f70
Russell Maycock <Russell.Maycock@pinport.io>
2020-10-29 16:08:32 +0000

    moved touch

--- run-tests.sh
+++ run-tests.sh
@@ -4,7 +4,7 @@ set -e
 # required to login to Azure to allow terratest test to run
 az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID 
 az account set -s $ARM_SUBSCRIPTION_ID
-touch ./test_output/russ.txt
+
 # check it's logged into the correct subscription before deploying the test infrastructure.
 SUBTRUE=$(az account show --query "isDefault")
 SUBID=$(az account show --query "id")
@@ -14,6 +14,7 @@ if [ "$SUBTRUE" -eq "true" ] -&& [ "$SUBID" -eq "$ARM_SUBSCRIPTION_ID" ] ; then
 # Run the tests
 cd ./test/
 mkdir test_output
+touch ./test_output/russ.txt
 go test -v -timeout 30m | tee test_output.log
 terratest_log_parser -testlog test_output.log -outputdir test_output
 
