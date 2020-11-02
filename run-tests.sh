5fb0ef549aabf4d212734b0bfbb38af6da1d3430
Russell Maycock <Russell.Maycock@pinport.io>
2020-11-02 15:44:22 +0000

    updated

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 # required to login to Azure to allow terratest test to run
 az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID 
@@ -9,16 +8,17 @@ az account set -s $ARM_SUBSCRIPTION_ID
 SUBTRUE=$(az account show --query "isDefault")
 SUBID=$(az account show --query "id")
 SUBID=$(sed -e 's/^"//' -e 's/"$//' <<<"$SUBID")
-if [ "$SUBTRUE" -eq "true" ] -&& [ "$SUBID" -eq "$ARM_SUBSCRIPTION_ID" ] ; then
-
+if [ "$SUBTRUE" == true ] && [ "$SUBID" == "$ARM_SUBSCRIPTION_ID" ] ; then
+echo "Correct Subscription - starting tests "
 # Run the tests
 cd ./test/
-mkdir test_output
+mkdir -p test_output
 touch ./test_output/russ.txt
 go test -v -timeout 30m | tee test_output.log
 terratest_log_parser -testlog test_output.log -outputdir test_output
 
 else
     echo "Not in the correct subscription, exiting"
-fi
+    exit 1
 
+fi
